26b322564256cb6987fc1f063c8f2e2d814aa014
andré <113362031+flopps213@users.noreply.github.com>
2023-06-08 11:36:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function comprarCoin() {
+    var valorCoins = document.getElementById("valorCoins").value;
+
+    alert("obrigado por comprar " + valorCoins + "capicoins :D");
+
+    document.getElementById("mensagem").innerHTML="você tem" + valorCoins + " capicoins";
+}
+
